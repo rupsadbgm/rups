7171ef5af716dfca94caa8cfa764a26ff682fa20
rupsadbgm <120558654+rupsadbgm@users.noreply.github.com>
2023-02-03 13:23:47 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,19 @@
+class AreaOfRectangle
+{
+	static String area()
+	{
+		String f="jspider";
+		int d=6;
+		int h=7;
+		double result= 0.5*d*h;
+		System.out.println(result);
+	return f;
+	}
+	public static void main(String[] args)
+	{
+		String d=area();
+		System.out.println(d);
+	}
+}
+
+
